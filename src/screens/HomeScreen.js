728b720839668ff1bba362f4cbfb2c211053bea3
Lucas Santos <santoslucas9956@gmail.com>
2025-06-09 23:04:59 -0300

    ID do Quiz

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import { View, Text, StyleSheet, Button, SafeAreaView, ImageBackground } from 'react-native';
 
 function HomeScreen({ navigation }) {
 
   function handleStartQuiz() {
-    navigation.navigate('Quiz');
+
+    navigation.navigate('Quiz', { quizId: 'q1' });
   }
 
   function handleGoToHistory() {
@@ -14,17 +14,16 @@ function HomeScreen({ navigation }) {
 
   return (
     <ImageBackground
-      source={require('../assets/logo.png')} 
+      source={require('../assets/logo.png')}
       resizeMode="cover"
-      style={styles.container} 
+      style={styles.container}
     >
-
       <View style={styles.overlay}>
         <Text style={styles.title}>Bem-vindo ao NeuroQuiz!</Text>
 
         <View style={styles.buttonContainer}>
           <Button
-            title="Iniciar Quiz"
+            title="Iniciar Quiz de Informática" 
             onPress={handleStartQuiz}
           />
           <Button
@@ -38,29 +37,25 @@ function HomeScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-
   container: {
-    flex: 1, 
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-
   overlay: {
     flex: 1,
     width: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.4)', 
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
   },
-
   title: {
-    fontSize: 32, 
+    fontSize: 32,
     fontWeight: 'bold',
-    color: '#FFFFFF', 
+    color: '#FFFFFF',
     textAlign: 'center',
-    marginBottom: 40, 
-
+    marginBottom: 40,
     textShadowColor: 'rgba(0, 0, 0, 0.75)',
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 10
